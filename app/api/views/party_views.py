b5cac04248b74330637d21ed268adfad683baab6
lenileiro <lenileiro@gmail.com>
2019-02-08 12:12:13 +0300

    [Feature #163549757] create poltical account endpoint togerther with endpoint test

--- app/api/views/party_views.py
+++ app/api/views/party_views.py
@@ -14,9 +14,9 @@ def delete_chat_messages(party_id):
     party_info = party.find_party(party_id)
     if party_info:
         party.delete_party(party_id)
-        return make_response(jsonify({"status": 204,
+        return make_response(jsonify({"status": 200 ,
                                       "data": [{
-                                          "message": "delete successful"}]})), 204
+                                          "message": "delete successful"}]})), 200
     else:
         return make_response(jsonify({"status": 400,
                                       "data": [{
@@ -34,13 +34,36 @@ def edit_chat_messages(party_id):
     party_info = party.find_party(party_id)
     if party_info:
         new_info = party.edit_party(party_id, name_data)
-        return make_response(jsonify({"status": 204,
+        return make_response(jsonify({"status": 200,
                                       "data": [{
                                           "id": party_id,
                                           "name": new_info["name"]
-                                          }]})), 204
+                                          }]})), 200
     else:
       return make_response(jsonify({"status": 400,
                                       "data": [{
                                           "message": "Id not found"}]})), 400
+
+###create party
+@parties_route.route('', methods=['POST']) 
+def create_chat_messages():
+    """"This route enables admin user
+      - to create political party"""
+    
+    data = request.get_json()
+    name_data = data.get('name')
+    address_data = data.get('hqAddress')
+    logo_data = data.get('logoUrl')
+
+    if name_data and address_data and logo_data:
+        new_info = party.create_party(name_data, address_data, logo_data)
+        return make_response(jsonify({"status": 200,
+                                      "data": [{
+                                          "id": new_info["id"],
+                                          "name": new_info["name"]
+                                          }]})), 200
+    else:
+      return make_response(jsonify({"status": 400,
+                                      "data": [{
+                                          "message": "some required fields missing"}]})), 400
 
--- app/test/test_party.py
+++ app/test/test_party.py
@@ -3,34 +3,63 @@
 import json
 from .basetest import BaseTest
 
-class TestDeleteRequest(BaseTest):
 
+class TestDeleteRequest(BaseTest):    
+    def test_invalid_delete_request(self):
+        response = self.invalid_delete_request()
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["status"], 400)
+        self.assertEqual(result["data"][0]["message"], "Id not found")
+    
     def test_valid_delete_request(self):
         response = self.valid_delete_request()
         result = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(result["status"], 204)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result["status"], 200)
         self.assertEqual(result["data"][0]["message"], "delete successful")
-    
+
+
+class TestPatchRequest(BaseTest):    
     def test_invalid_delete_request(self):
-        response = self.invalid_delete_request()
+        response = self.invalid_patch_request()
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 400)
         self.assertEqual(result["status"], 400)
         self.assertEqual(result["data"][0]["message"], "Id not found")
 
-class TestPatchRequest(BaseTest):
-
-    def test_valid_delete_request(self):
+    def test_valid_patch_request(self):
         response = self.valid_patch_request()
         result = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(result["status"], 204)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result["status"], 200)
         self.assertEqual(result["data"][0]["name"], "new political party name")
-    
-    def test_invalid_delete_request(self):
-        response = self.invalid_delete_request()
+
+class TestPostRequest(BaseTest):    
+    def test_valid_post_request(self):
+        response = self.valid_post_request()
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result["status"], 200)
+        self.assertEqual(result["data"][0]["name"], "party C")
+
+    def test_invalid_post_request_1(self):
+        response = self.invalid_post_request_1()
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 400)
         self.assertEqual(result["status"], 400)
-        self.assertEqual(result["data"][0]["message"], "Id not found")
+        self.assertEqual(result["data"][0]["message"], "some required fields missing")
+    
+    def test_invalid_post_request_2(self):
+        response = self.invalid_post_request_2()
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["status"], 400)
+        self.assertEqual(result["data"][0]["message"], "some required fields missing")
+    
+    def test_invalid_post_request_3(self):
+        response = self.invalid_post_request_3()
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["status"], 400)
+        self.assertEqual(result["data"][0]["message"], "some required fields missing")
