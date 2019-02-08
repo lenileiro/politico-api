9ba884b84b4619a41cc03155539c744dfdff6c09
lenileiro <lenileiro@gmail.com>
2019-02-08 17:30:35 +0300

    [Chore #163821599] remove capitalization on app variable

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 from app import create_app
 config_name = os.getenv("FLASK_ENV",'production')
 
-APP = create_app(config_name)
+app = create_app(config_name)
 
 if __name__ == "__main__":
-    APP.run()
+    app.run()
