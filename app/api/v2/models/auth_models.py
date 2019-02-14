ddf647971561ad79f9dda7706dd9ebdf595690ec
lenileiro <lenileiro@gmail.com>
2019-02-15 00:09:37 +0300

    [Finishes #163630790] completes login api endpoint

--- app/api/v2/models/auth_models.py
+++ app/api/v2/models/auth_models.py
@@ -8,41 +8,50 @@ class AuthModel:
         self.db = conn()
 
     def login_a_user(self, national_id, password):
-        encripted_password = generate_password_hash(password)
-       
         cursor = self.db.cursor()
         cursor.execute(
             """SELECT * FROM politico.user where national_id={} """.format(national_id)
         )
         user = cursor.fetchone()
-        stored_passord = user[8]
 
-        results = check_password_hash(stored_passord, password)
+        if user:
+            ### User not empty 
+            token = self.generate_jwt_token({"id":national_id})
+            strinfied = "{}".format(token)
 
-        data = {
-            "token": "45erkjherht45495783",
-            "user": {
-            "id": user[1],
-            "firstname": user[2],
-            "lastname": user[3],
-            "othername": user[4],
-            "email": user[5],
-            "phoneNumber": user[6],
-            "passportUrl": user[9],
-            "isAdmin": user[7]
+            data = {
+                "token": strinfied,
+                "user": {
+                "id": user[1],
+                "firstname": user[2],
+                "lastname": user[3],
+                "othername": user[4],
+                "email": user[5],
+                "phoneNumber": user[6],
+                "passportUrl": user[9],
+                "isAdmin": user[7]
+                }
             }
-        }
-        if results:
-            return data
+
+            stored_passord = user[8]
+            results = check_password_hash(stored_passord, password)
+
+            if results:
+                return data
+            else:
+                return {
+                "message": "Invalid Password"
+            }
         else:
-            pass
+            return {
+                "message": "User not registered"
+            }
+
         cursor.close()
-        return encripted_password
     
-    """ def generate_jwt_token(self, payload):
+    def generate_jwt_token(self, payload):
         fileDir = os.path.dirname(os.path.realpath('__file__'))
         filename = os.path.join(fileDir, './utils/keys/jwt-key')
         secret_key = open(filename).read()
         token = jwt.encode(payload, secret_key, algorithm='HS256').decode('utf-8')
-        return token """
-        
+        return token
